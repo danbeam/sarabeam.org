4e8a6be3952ba372e8fc7ac557ce9c03c80d4b16
Dan Beam <dan@danbeam.org>
2010-12-15 11:41:20 -0800

    tweaking run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # run WWW::Mechanize test
-#prove tests/*.t || exit 1;
+prove tests/*.t || exit 1;
 
 # run client-side JS GUI tests
 export DISPLAY=":1.0";
-pidof Xvfb || ( startx -- `which Xvfb` :1 -screen 0 1024x768x24 & ) >> /var/log/xvfb.log 2>&1;
+pgrep Xvfb || ( startx -- `which Xvfb` :1 -screen 0 1024x768x24 & ) >> /var/log/Xvfb.log 2>&1;
 java -jar /usr/lib/selenium-server-1.0.3/selenium-server.jar -htmlSuite "*firefox" "http://sarabeam.org" $WORKSPACE/tests/test_suite.html $WORKSPACE/results.html || exit 1;
