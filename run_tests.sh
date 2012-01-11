f9e21bb231c0807d9043d0b1b5d85010ba731cf5
Dan Beam <dan@danbeam.org>
2012-01-11 02:44:44 -0800

    switching sarabeam.org -> sarabeam.dbhn

--- run_tests.sh
+++ run_tests.sh
@@ -6,4 +6,4 @@ prove tests/*.t || exit 1;
 # run client-side JS GUI tests
 export DISPLAY=":1.0";
 pgrep Xvfb || ( startx -- `which Xvfb` :1 -screen 0 1024x768x24 & ) >> /var/log/Xvfb.log 2>&1;
-java -jar /usr/lib/selenium-server-1.0.3/selenium-server.jar -htmlSuite "*firefox" "http://sarabeam.org" $WORKSPACE/tests/test_suite.html $WORKSPACE/results.html || exit 1;
+java -jar /usr/lib/selenium-server-1.0.3/selenium-server.jar -htmlSuite "*firefox" "http://sarabeam.dbhn" $WORKSPACE/tests/test_suite.html $WORKSPACE/results.html || exit 1;
